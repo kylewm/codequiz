d9a31c90510f2c5c3850cf91ae66e1666c061b08
Kyle Mahan <kyle.mahan@gmail.com>
2014-02-03 11:58:55 -0800

    Fix crash when adding new candidates

--- codequiz.py
+++ codequiz.py
@@ -156,22 +156,29 @@ def candidate_view(candidate_id):
             candidate.notify_emails = request.form['notify_emails']
             candidate.problems = []
             problem_ids = request.form.getlist('problem')
-            idx = 0
+
             for problem_id in problem_ids:
                 selected_problem = Problem.query.\
                     filter(Problem.id == problem_id).\
                     one()
                 candidate.problems.append(selected_problem)
                 
-                db.session.execute(candidate_problems.update().\
-                                   where(candidate_problems.columns.problem_id==problem_id).\
-                                   where(candidate_problems.columns.candidate_id==candidate_id).\
-                                   values(position=idx))
-                idx += 1
                 
             if not candidate.id:
                 db.session.add(candidate)
+                
             db.session.commit()
+
+            idx = 0
+            for problem_id in problem_ids:
+                db.session.execute(candidate_problems.update().\
+                                   where(candidate_problems.columns.problem_id==problem_id).\
+                                   where(candidate_problems.columns.candidate_id==candidate.id).\
+                                   values(position=idx))
+                idx += 1
+
+            db.session.commit()
+            
             return redirect(url_for('candidate_view', candidate_id=candidate.id, action='view'))
 
         else:
